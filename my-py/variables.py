54ffbddfcfe785052ac2d99337c685781defee2a
freeman <98413109+free-man11@users.noreply.github.com>
2022-02-08 23:59:39 +0000

    var

---
+++ my-py/variables.py
@@ -0,0 +1,8 @@
+print(len(input("What is your name?")))
+
+# instead of using the print function, the variable function comes in😁🙌🏾
+
+name = input("What is your name?")
+length = len(name)
+
+print(length)
