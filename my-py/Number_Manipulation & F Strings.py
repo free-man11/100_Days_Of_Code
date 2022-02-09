a62cd3b697063bebb00c24265e789f50a5ef01df
freeman <98413109+free-man11@users.noreply.github.com>
2022-02-09 01:17:42 +0000

    Numb_mani & F_str

---
+++ my-py/Number_Manipulation & F Strings.py
@@ -0,0 +1,45 @@
+# want to cut terminating decimals into a whole numb,
+
+print(int(8/3))
+
+# we can also use floor division method instead of inserting an (int funct)
+print(8//3)
+
+# while if you to run the numbers,add the number of deci you want to run, then here you go
+print(round(8/3,2))
+
+
+
+
+
+
+
+# If you to Manipulate series of values in programming with series of addition;then let's go
+
+score = 0
+
+# user scores a point
+
+score += 3
+print(score)
+
+
+
+
+
+
+
+
+# F - STRINGS
+
+# helps to combine string,int,float & boolean (all the data types)
+
+
+score = 0
+height = 1.8
+isWinning = True
+
+# F-strings
+
+print(f"your score is {score}, your height is {height}, your are winning is {isWinning} ")
+
