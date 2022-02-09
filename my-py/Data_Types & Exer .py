2caaf7fd4ce9ad1bda6d8bf02139f09dbaf25a0e
freeman <98413109+free-man11@users.noreply.github.com>
2022-02-09 01:12:34 +0000

    Data types

---
+++ my-py/Data_Types & Exer .py
@@ -0,0 +1,27 @@
+
+# STRINGS
+
+
+# we now use an indend to position the numb a word has
+print("hello"[0])
+
+
+# strings don't operate with operational signs
+print("123" + "345")
+
+
+
+# INTEGERS---Numbers without decimal points
+print(123 + 234)
+
+123_456_789
+
+
+# FLOAT---Numbers with decimal points
+print(3.22)
+
+
+# BOOLEAN---Always comes with True & False
+
+True
+False
