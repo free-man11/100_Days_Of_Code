d8290842d7ccf7b002336ccc610d527f49bfa568
freeman <98413109+free-man11@users.noreply.github.com>
2022-02-09 01:15:52 +0000

    Math_operations

---
+++ my-py/Mathematical_Operations.py
@@ -0,0 +1,21 @@
+3 + 5
+7 - 4
+3 * 2
+6 / 3
+2 ** 3
+
+
+# PEMDASLR
+
+# parenthesis ()
+# exponent (**)
+# Multiplication (*)
+# division (/)
+# addition (+)
+# subtraction (-)
+
+# use thonny to understand it deeply
+
+print(3 * 3 + 3 /3 - 3)
+
+print(3 * (3 + 3) / 3 - 3)
