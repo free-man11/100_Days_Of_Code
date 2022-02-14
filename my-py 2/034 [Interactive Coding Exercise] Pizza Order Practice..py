55b70f7f576cb4e42025a8a6d3957cd464764fcf
freeman <98413109+free-man11@users.noreply.github.com>
2022-02-14 14:51:23 +0000

    pizza order

---
+++ my-py 2/034 [Interactive Coding Exercise] Pizza Order Practice..py
@@ -0,0 +1,35 @@
+
+
+# HURRAY I CREATED A PIZZA DELIVERY BOT
+
+print("Welcome to Free Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L \n")
+add_pepperoni = input("Do you want pepperoni? Y or N \n")
+extra_cheese = input("Do you want extra cheese? Y or N \n")
+
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+else:
+    bill += 0
+
+    print(f"Your final bill is ${bill}")
+
+
+
+
+
