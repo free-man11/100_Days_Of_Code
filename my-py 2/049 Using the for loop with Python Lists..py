d71b4356f1df2fbe1f4244f8d18c04edb09e4489
freeman <98413109+free-man11@users.noreply.github.com>
2022-02-22 02:10:40 +0000

    For Loop In Python

---
+++ my-py 2/049 Using the for loop with Python Lists..py
@@ -0,0 +1,15 @@
+# USING LOOPS WE CARE ABOUT THE INDENTATIONS
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+
+
+# WHAT WILL THIS BE IF WE ADD(   + "PIE" )
+
+    print(fruit + "Pie")
+
+# IF WE THEREBY WRITE THE PRINT OUTSIDE THE FOE LOOP; THEN IT WILL PRINT ONLY ONE
+
+print(fruits)
+
